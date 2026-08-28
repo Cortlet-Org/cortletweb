54c49750b360f72e22f158a3f10805fb1028b47c
Cortlet-Org <kkuioslaptop@Krishnas-MacBook-Pro.local>
2026-08-28 17:52:05 +0530

    infra: externalize react-email packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    serverExternalPackages: [
+        "@react-email/components",
+        "@react-email/render"
+    ]
 };
 
 export default nextConfig;
